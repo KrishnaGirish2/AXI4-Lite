e82c08f92a8751f5195364dae2f898bed6c7f584
KrishnaGirish2 <krishnagirish1458@gmail.com>
2026-09-13 23:03:18 +0530

    Refactor axi4lite_config class formatting

--- src/testbench/axi4lite_config.sv
+++ src/testbench/axi4lite_config.sv
@@ -1,9 +1,9 @@
 class axi4lite_config extends uvm_object;
-`uvm_object_utils(axi4lite_config)
-virtual axi4lite_if vif;
-uvm_active_passive_enum input_agent_is_active=UVM_ACTIVE;
-uvm_active_passive_enum output_agent_is_active=UVM_PASSIVE;
-function new(string name="axi4lite_config");
-super.new(name);
-endfunction
+  `uvm_object_utils(axi4lite_config)
+  virtual axi4lite_if vif;
+  uvm_active_passive_enum input_agent_is_active=UVM_ACTIVE;
+  uvm_active_passive_enum output_agent_is_active=UVM_PASSIVE;
+  function new(string name="axi4lite_config");
+    super.new(name);
+  endfunction
 endclass
