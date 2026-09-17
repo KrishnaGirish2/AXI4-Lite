bbfa577928b88d44b64df74e1910fe1eda8b1278
KrishnaGirish2 <krishnagirish1458@gmail.com>
2026-09-17 14:11:53 +0530

    Refactor input_monitor for improved functionality
    Refactor input_monitor to enhance input collection and logging.

--- src/testbench/axi4lite_input_monitor.sv
+++ src/testbench/axi4lite_input_monitor.sv
@@ -1,91 +1,103 @@
 class input_monitor extends uvm_monitor;
-    `uvm_component_utils(input_monitor)
-    uvm_analysis_port#(trans) inp_monitor_port;
-    virtual axi4lite_if.inp_mon_mp vif;
-    axi4lite_config m_cfg;
-    trans drv2mon;
-    function new(string name="input monitor",uvm_component parent);
-        super.new(name,parent);
-    endfunction
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
-        if(!uvm_config_db#(axi4lite_config)::get(this,"","axi4lite_config",m_cfg))
-            `uvm_fatal(get_type_name(),"Input monitor failed")
-        inp_monitor_port=new("inp_monitor_port",this);
-    endfunction
-    function void connect_phase(uvm_phase phase);
-        super.connect_phase(phase);
-        vif=m_cfg.vif;
-    endfunction
-    task run_phase(uvm_phase phase);
-        forever begin
-            drv2mon=trans::type_id::create("drv2mon");
-            `uvm_info("Input monitor check","Before collect input monitor",UVM_NONE)
-            collect_input_monitor();
-            `uvm_info("Input monitor check","After collect input monitor",UVM_NONE)
-            inp_monitor_port.write(drv2mon);
-            @(vif.inp_mon_cb);
-            `uvm_info("Input monitor",$sformatf("Input Monitor\n%s",drv2mon.sprint()),UVM_NONE)
-        end
-    endtask
-    virtual task collect_input_monitor();
-        forever begin
-            @(vif.inp_mon_cb);
-            $display("%0t MONITOR: AWVALID=%0d AWREADY=%0d",$time,vif.inp_mon_cb.AWVALID,vif.inp_mon_cb.AWREADY);
-            if ((vif.inp_mon_cb.AWVALID && vif.inp_mon_cb.AWREADY) && !(vif.inp_mon_cb.WVALID && vif.inp_mon_cb.WREADY)) begin
-                drv2mon.AWADDR=vif.inp_mon_cb.AWADDR;
-                drv2mon.AWPROT=vif.inp_mon_cb.AWPROT;
-                drv2mon.write_read=1;
-                `uvm_info("MON_WAIT","Waiting for W handshake",UVM_NONE)
-                wait(vif.inp_mon_cb.WVALID && vif.inp_mon_cb.WREADY);
-                `uvm_info("MON_WAIT","Done for W handshake",UVM_NONE)
-                drv2mon.WDATA=vif.inp_mon_cb.WDATA;
-                drv2mon.WSTRB=vif.inp_mon_cb.WSTRB;
-                drv2mon.ARESETn=vif.inp_mon_cb.ARESETn;
-                `uvm_info("MON_DEBUG","Returning from AW branch",UVM_NONE)
-                return;
-            end
+	`uvm_component_utils(input_monitor)
+	uvm_analysis_port#(trans) inp_monitor_port;
+	virtual axi4lite_if.inp_mon_mp vif;
+	axi4lite_config m_cfg;
+	trans drv2mon;
+	function new(string name="input monitor",uvm_component parent);
+		super.new(name,parent);
+	endfunction
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		if(!uvm_config_db#(axi4lite_config)::get(this,"","axi4lite_config",m_cfg))
+			`uvm_fatal(get_type_name(),"Input monitor failed")
+		inp_monitor_port=new("inp_monitor_port",this);
+	endfunction
+	function void connect_phase(uvm_phase phase);
+		super.connect_phase(phase);
+		vif=m_cfg.vif;
+	endfunction
+	task run_phase(uvm_phase phase);
+	forever begin
+		drv2mon=trans::type_id::create("drv2mon");
+		`uvm_info("Input monitor check","Before collect input monitor",UVM_NONE)
+		`uvm_info("Input monitor check",$sformatf("ARVALID=%0d ARREADY=%0d,AWVALID=%0d AWREADY=%0d,WVALID=%0d WREADY=%0d",vif.inp_mon_cb.ARVALID,vif.inp_mon_cb.ARREADY,vif.inp_mon_cb.AWVALID,vif.inp_mon_cb.AWREADY,vif.inp_mon_cb.WVALID,vif.inp_mon_cb.WREADY),UVM_NONE)
+		collect_input_monitor();
+		`uvm_info("Input monitor check","After collect input monitor",UVM_NONE)
+		inp_monitor_port.write(drv2mon);
+		@(vif.inp_mon_cb);
+		`uvm_info("Input monitor",$sformatf("Input Monitor\n%s",drv2mon.sprint()),UVM_NONE)
+	end
+	endtask
+	virtual task collect_input_monitor();
+	forever
+	 begin
+		@(vif.inp_mon_cb);
+		`uvm_info("Input monitor check",$sformatf("ARVALID=%0d ARREADY=%0d,AWVALID=%0d AWREADY=%0d,WVALID=%0d WREADY=%0d",vif.inp_mon_cb.ARVALID,vif.inp_mon_cb.ARREADY,vif.inp_mon_cb.AWVALID,vif.inp_mon_cb.AWREADY,
+		vif.inp_mon_cb.WVALID,vif.inp_mon_cb.WREADY),UVM_NONE)
+		if((vif.inp_mon_cb.AWVALID && vif.inp_mon_cb.AWREADY) && (vif.inp_mon_cb.WREADY && vif.inp_mon_cb.WVALID) && (vif.inp_mon_cb.ARVALID && vif.inp_mon_cb.ARREADY)) begin
+			`uvm_info("Input monitor ","Simultaneous check",UVM_NONE)
+			drv2mon.AWADDR=vif.inp_mon_cb.AWADDR;
+			drv2mon.AWPROT=vif.inp_mon_cb.AWPROT;
+			drv2mon.WDATA=vif.inp_mon_cb.WDATA;
+			drv2mon.WSTRB=vif.inp_mon_cb.WSTRB;
+			drv2mon.ARESETn=vif.inp_mon_cb.ARESETn;
+			drv2mon.ARADDR=vif.inp_mon_cb.ARADDR;
+			drv2mon.ARPROT=vif.inp_mon_cb.ARPROT;
+			drv2mon.write_read=1;
+			drv2mon.write_order=3'd4;
+			return;
+		end
 
-            else if(!(vif.inp_mon_cb.AWVALID && vif.inp_mon_cb.AWREADY) && (vif.inp_mon_cb.WVALID && vif.inp_mon_cb.WREADY)) begin
-                drv2mon.WDATA=vif.inp_mon_cb.WDATA;
-                drv2mon.WSTRB=vif.inp_mon_cb.WSTRB;
-                drv2mon.write_read=1;
-                `uvm_info("MON_WAIT","Waiting for AW handshake",UVM_NONE)
-                wait(vif.inp_mon_cb.AWVALID && vif.inp_mon_cb.AWREADY);
-                `uvm_info("MON_WAIT","Done for AW handshake",UVM_NONE)
-                drv2mon.AWADDR=vif.inp_mon_cb.AWADDR;
-                drv2mon.AWPROT=vif.inp_mon_cb.AWPROT;
-                drv2mon.ARESETn=vif.inp_mon_cb.ARESETn;
-                `uvm_info("MON_DEBUG","Returning from W branch",UVM_NONE)
-                return;
-            end
+		else if ((vif.inp_mon_cb.AWVALID && vif.inp_mon_cb.AWREADY) && !(vif.inp_mon_cb.WVALID && vif.inp_mon_cb.WREADY)) begin
+			drv2mon.AWADDR=vif.inp_mon_cb.AWADDR;
+			drv2mon.AWPROT=vif.inp_mon_cb.AWPROT;
+			drv2mon.write_read=1;
+			drv2mon.write_order=0;
+			`uvm_info("MON_WAIT","Waiting for W handshake",UVM_NONE)
+			wait(vif.inp_mon_cb.WVALID && vif.inp_mon_cb.WREADY);
+			`uvm_info("MON_WAIT","Done for W handshake",UVM_NONE)
+			drv2mon.WDATA=vif.inp_mon_cb.WDATA;
+			drv2mon.WSTRB=vif.inp_mon_cb.WSTRB;
+			drv2mon.ARESETn=vif.inp_mon_cb.ARESETn;
+			`uvm_info("MON_DEBUG","Returning from AW branch",UVM_NONE)
+			return;
+		end
 
-            else if((vif.inp_mon_cb.AWVALID && vif.inp_mon_cb.AWREADY) && (vif.inp_mon_cb.WVALID && vif.inp_mon_cb.WREADY)) begin
-                drv2mon.WDATA=vif.inp_mon_cb.WDATA;
-                drv2mon.WSTRB=vif.inp_mon_cb.WSTRB;
-                drv2mon.write_read=1;
-                drv2mon.AWADDR=vif.inp_mon_cb.AWADDR;
-                drv2mon.AWPROT=vif.inp_mon_cb.AWPROT;
-                drv2mon.ARESETn=vif.inp_mon_cb.ARESETn;
-                `uvm_info("MON_DEBUG","Returning from AW and W branch",UVM_NONE)
-                return;    
-            end
+		else if(!(vif.inp_mon_cb.AWVALID && vif.inp_mon_cb.AWREADY) && (vif.inp_mon_cb.WVALID && vif.inp_mon_cb.WREADY)) begin
+			drv2mon.WDATA=vif.inp_mon_cb.WDATA;
+			drv2mon.WSTRB=vif.inp_mon_cb.WSTRB;
+			drv2mon.write_read=1;
+			drv2mon.write_order=1;
+			`uvm_info("MON_WAIT","Waiting for AW handshake",UVM_NONE)
+			wait(vif.inp_mon_cb.AWVALID && vif.inp_mon_cb.AWREADY);
+			`uvm_info("MON_WAIT","Done for AW handshake",UVM_NONE)
+			drv2mon.AWADDR=vif.inp_mon_cb.AWADDR;
+			drv2mon.AWPROT=vif.inp_mon_cb.AWPROT;
+			drv2mon.ARESETn=vif.inp_mon_cb.ARESETn;
+			`uvm_info("MON_DEBUG","Returning from W branch",UVM_NONE)
+			return;
+		end
 
-            else if(vif.inp_mon_cb.ARVALID && vif.inp_mon_cb.ARREADY) begin
-                drv2mon.ARADDR=vif.inp_mon_cb.ARADDR;
-                drv2mon.ARPROT=vif.inp_mon_cb.ARPROT;
-                drv2mon.write_read=0;
-                drv2mon.ARESETn=vif.inp_mon_cb.ARESETn;
-                return;
-            end
-        end
-    endtask
+		else if((vif.inp_mon_cb.AWVALID && vif.inp_mon_cb.AWREADY) && (vif.inp_mon_cb.WVALID && vif.inp_mon_cb.WREADY)) begin
+			drv2mon.WDATA=vif.inp_mon_cb.WDATA;
+			drv2mon.WSTRB=vif.inp_mon_cb.WSTRB;
+			drv2mon.write_read=1;
+			drv2mon.write_order=3;
+			drv2mon.AWADDR=vif.inp_mon_cb.AWADDR;
+			drv2mon.AWPROT=vif.inp_mon_cb.AWPROT;
+			drv2mon.ARESETn=vif.inp_mon_cb.ARESETn;
+			`uvm_info("MON_DEBUG","Returning from AW and W branch",UVM_NONE)
+			return;
+		end
+
+		else if(vif.inp_mon_cb.ARVALID && vif.inp_mon_cb.ARREADY) begin
+			drv2mon.ARADDR=vif.inp_mon_cb.ARADDR;
+			drv2mon.ARPROT=vif.inp_mon_cb.ARPROT;
+			drv2mon.write_read=0;
+			drv2mon.ARESETn=vif.inp_mon_cb.ARESETn;
+			return;
+		end
+	end
+	endtask
 endclass
-
-
-
-
-
-
-
-
